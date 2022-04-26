f7100ea7bd653cb4c67a889be8b503700e2cabd9
aytekin_desktop <mjeong827@gmail.com>
2022-04-27 00:33:18 +0900

    admin페이지 설정

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -6,12 +6,19 @@ from django.db import models
 
 class Fcuser(models.Model):
     username = models.CharField(max_length=32,
-                                verbose_name='사용자명')
+                                verbose_name='사용자명')# UI에 표시될 내용
 
     password = models.CharField(max_length=64,
-                                verbose_name='비밀번호')
-    registered_dttm = models.DateTimeField(auto_now_add=True,
-                                            verbose_name='등록시간')
+                                verbose_name='비밀번호')# UI에 표시될 내용
+
+    registered_dttm = models.DateTimeField(auto_now_add=True,# 장고에 auto_now라는 기능이 있어서 그 시간대 바로 등록 가능.
+                                            verbose_name='등록시간')# UI에 표시될 내용
+
+    # 클래스의 이름과 저장위치를 알고싶을 때 print(클래스객체)이런식으로 확인할 수 있는데
+    # 이때 object클래스의 __str__매서드가 호출되어 반환된 문자열 정보이다.
+    # Fcuser라는 클래스가 문자열로 출력될 때 어떻게 출력시킬지 정해주는 파이썬 내장함수로 원하는 값으로 정해주면 그 값을 출력해주도록 설정할 수 있다.
+    def __str__(self):
+        return self.username 
 
     # 테이블 명을 별도로 지정하고 싶을 때 
     class Meta:
