29bdea83649e15fce06f2c2e9d4f6d4f52a399a0
aytekin_desktop <mjeong827@gmail.com>
2022-04-27 00:45:06 +0900

    회원가입 페이지 생성

--- fc_community/fcuser/admin.py
+++ fc_community/fcuser/admin.py
@@ -5,5 +5,7 @@ from .models import Fcuser
 
 class FcuserAdmin(admin.ModelAdmin):
     list_display = ('username','password','registered_dttm')
+    # fcuser admin페이지에서 칼럼에 나타낼 값들을 정해줄 수 있따.
 
-admin.site.register(Fcuser, FcuserAdmin)
+# admin/fc_user(앱이름)/fcuser로 라우팅될때 FCuserAdmin이라는 클래스가 리턴되도록
+admin.site.register(Fcuser, FcuserAdmin) 
