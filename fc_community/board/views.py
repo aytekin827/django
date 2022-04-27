0972cbdec5ff7543b5cfa9050965bbd705db694c
aytekin_desktop <mjeong827@gmail.com>
2022-04-27 19:44:20 +0900

    게시판만들기

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Board
 
 # Create your views here.
+def board_list(request):
+    boards = Board.objects.all().order_by('-id') # '-'는 역순이라는 뜻
+    return render(request,'board_list.html',{'boards':boards})    
