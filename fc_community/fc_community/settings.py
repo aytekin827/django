e7d2dcf31e4a22b90bdbd48efbc54e80a852588a
aytekin_desktop <mjeong827@gmail.com>
2022-04-28 11:10:06 +0900

    앱 배포 - django설정

--- fc_community/fc_community/settings.py
+++ fc_community/fc_community/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5_7+)a()_ei1@civ8hl*y4n@u8=po1q!t161m9p9sp14i5iaun'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'alswo7172.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
